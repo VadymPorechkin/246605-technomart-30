35548b10dd0bd93d86b68d4c8c2ea8c9c0dd3756
VadymPorechkin <v.porechkin@gmail.com>
2021-03-21 14:57:34 +0200

    Добавляет изменения

---
+++ js/map.js
@@ -0,0 +1,27 @@
+const map = document.querySelector('.modal-map');
+const mapOpenButton = document.querySelector('.contacts-map');
+const mapCloseButton = map.querySelector('.modal-close');
+
+mapOpenButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  blackout.classList.add('active');
+
+  map.classList.add('modal-show');
+});
+
+mapCloseButton.addEventListener('click', function (e) {
+  blackout.classList.remove('active');
+
+  map.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode === 27) {
+    if (map.classList.contains('modal-show')) {
+      e.preventDefault();
+      blackout.classList.remove('active');
+
+      map.classList.remove('modal-show');
+    }
+  }
+});
